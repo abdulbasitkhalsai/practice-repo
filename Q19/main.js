e2cccb2031c832d1df06be09d0281ecca1622036
Abdul Basit Khalsai <abdulbasitkhalsai@gmail.com>
2024-06-06 21:14:41 +0500

    22 Questions completed

--- Q19/main.js
+++ Q19/main.js
@@ -1,6 +1,5 @@
+"use strict";
 //Assignemnt # 19
-
 // Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
-
-let guestArray = ["Abdul Basit", "Shoaib", "Shaheen", "Jameel", "Kaleem"]
+let guestArray = ["Abdul Basit", "Shoaib", "Shaheen", "Jameel", "Kaleem"];
 console.log(`We have invited ${guestArray.length} persons at dinner today`);
--- Q21/main.js
+++ Q21/main.js
@@ -1,11 +1,10 @@
+"use strict";
 // They think of something you could store in a TypeScript Object. 
 // Write a program that creates Objects containing these items.
-
 let car = {
     model: "Suzuki Alto",
     modelYear: "2021",
     color: "White",
     engine: "660cc"
-}
-
-console.log(`I have ${car.model} - ${car.modelYear} having ${car.color} color. Its engine capacity is ${car.engine}`);
+};
+console.log(`I have ${car.model} - ${car.modelYear} having ${car.color}. Its engine capacity is ${car.engine}`);
