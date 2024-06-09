64e40cf69f982743ed2ed3d9c77e5f4adbac8e8e
Abdul Basit Khalsai <abdulbasitkhalsai@gmail.com>
2024-06-09 21:02:07 +0500

    33 Assignment Completed

--- Q30/main.js
+++ Q30/main.js
@@ -1,23 +1,23 @@
+"use strict";
 // Hello Admin: Make a array of five or more usernames, including the name 'admin'. Imagine you are writing code that will print a greeting to each user after they log in to a website. Loop through the array, and print a greeting to each user:
 // • If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report?
-
 // • Otherwise, print a generic greeting, such as Hello Eric, thank you for logging in again.
-
-let userName: string[] = ["Abdul", "Ali", "Ahmed", "Admin", "Jameel"];
-
+let userName = ["Abdul", "Ali", "Ahmed", "Admin", "Jameel"];
 // Simple For Loop
 for (let i = 0; i < userName.length; i++) {
-  if (userName[i] == "Admin") {
-    console.log("Hello Admin, would you like to see a status report?");
-  } else {
-    console.log(`Hello ${userName[i]}, thank you for logging in again.`);
-  }
+    if (userName[i] == "Admin") {
+        console.log("Hello Admin, would you like to see a status report?");
+    }
+    else {
+        console.log(`Hello ${userName[i]}, thank you for logging in again.`);
+    }
 }
 //For Each loop
 userName.forEach((userName) => {
-  if (userName == "Admin") {
-    console.log("Hello Admin, would you like to see a status report?");
-  } else {
-    console.log(`Hello! ${userName}, thank you for logging in again.`);
-  }
+    if (userName == "Admin") {
+        console.log("Hello Admin, would you like to see a status report?");
+    }
+    else {
+        console.log(`Hello! ${userName}, thank you for logging in again.`);
+    }
 });
--- Q32/main.js
+++ Q32/main.js
@@ -1,37 +1,31 @@
+"use strict";
 // // Assignment # 32
 // Checking Usernames: Do the following to create a program that simulates how websites ensure that everyone has a unique username.
 // • Make a list of five or more usernames called current_users.
-
 // • Make another list of five usernames called new_users. Make sure one or two of the new usernames are also in the current_users list.
-
 // • Loop through the new_users list to see if each new username has already been used. If it has, print a message that the person will need to enter a new username. If a username has not been used, print a message saying that the username is available.
-
 // • Make sure your comparison is case insensitive. If 'John' has been used, 'JOHN' should not be accepted
-
 // Solution # 1
 let currentUsers = ["Ali", "Junaid", "Shoaib", "Jameel", "Nadeem"];
 let newUser = ["Jamshed", "ALI", "Saleem", "jameel", "Kaleem"];
 let lowerCaseUsers = currentUsers.map((user) => user.toLowerCase());
 for (let user of newUser) {
-  if (lowerCaseUsers.includes(user.toLowerCase())) {
-    console.log(`${user} is already taken, please choose another username`);
-  } else {
-    console.log(`${user} is available`);
-  }
+    if (lowerCaseUsers.includes(user.toLowerCase())) {
+        console.log(`${user} is already taken, please choose another username`);
+    }
+    else {
+        console.log(`${user} is available`);
+    }
 }
 // Solution # 2
-let current_users: string[] = ["user1", "ADmin", "user3", "user4", "user5"];
-let new_users: string[] = ["User1", "User6", "user7", "admin", "User9"];
-
+let current_users = ["user1", "ADmin", "user3", "user4", "user5"];
+let new_users = ["User1", "User6", "user7", "admin", "User9"];
 new_users.forEach((newUser) => {
-  if (
-    current_users.some(
-      (currentUser) => currentUser.toLowerCase() === newUser.toLowerCase(),
-    )
-  ) {
-    console.log(`${newUser} will need to enter a new username.`);
-    console.log(current_users);
-  } else {
-    console.log(`${newUser} is available.`);
-  }
+    if (current_users.some((currentUser) => currentUser.toLowerCase() === newUser.toLowerCase())) {
+        console.log(`${newUser} will need to enter a new username.`);
+        console.log(current_users);
+    }
+    else {
+        console.log(`${newUser} is available.`);
+    }
 });
