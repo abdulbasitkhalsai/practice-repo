7338b8f3f6a0ec2c498adf5eef794f886658657a
Abdul Basit Khalsai <abdulbasitkhalsai@gmail.com>
2024-06-16 20:56:31 +0500

    36 Assignment Done

--- Q36/main.js
+++ Q36/main.js
@@ -1,10 +1,7 @@
+"use strict";
 // // Assignment # 36
 // T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function.
-
-function make_shirt(size: number, message: string) {
-    console.log(
-      `You have ordered a T-Shirt having size : ${size} with the message ${message}`,
-    );
-  }
-  
-  make_shirt(10, "I love Typescript");
+function make_shirt(size, message) {
+    console.log(`You have ordered a T-Shirt having size : ${size} with the message ${message}`);
+}
+make_shirt(10, "I love Typescript");
